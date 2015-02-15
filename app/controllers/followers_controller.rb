7b6dd3b55bcd8170124fadb2ebd4ec134be167c0
John Mahowald <john.mahowald@gmail.com>
2015-02-14 19:44:18 -0800

    creating create method

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -1,2 +1,10 @@
 class FollowersController < ApplicationController
+  def create
+  end
+
+  private
+
+  def follower_params
+    params.require(:follower).permit(:user_id, :followee_id)
+  end
 end
