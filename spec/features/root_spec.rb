53924dd89360ae0a40b585f74ef2ea6e77d72235
John Mahowald <john.mahowald@gmail.com>
2015-03-11 00:48:01 -0700

    refactoring root user

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe "Signing In" do
-  it "can sign a user in" do
-    @user = create :user
+RSpec.describe "Root View" do
+  describe "Sessions" do
+    it "can sign a user in" do
+      @user = create :user
 
-    expect(page.has_button? "Sign out").to be false
+      expect(page.has_button? "Sign out").to be false
 
-    visit("/")
-    fill_in("session[username]", with: @user.username)
-    fill_in("session[password]", with: @user.password)
-    click_button("Log in")
+      visit("/")
+      fill_in("session[username]", with: @user.username)
+      fill_in("session[password]", with: @user.password)
+      click_button("Log in")
 
-    expect(page.has_button? "Sign out").to be true
-    expect(page).to have_content @user.username
+      expect(page.has_button? "Sign out").to be true
+      expect(page).to have_content @user.username
+    end
   end
 end
