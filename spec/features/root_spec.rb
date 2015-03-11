6c23116c9e0410d69d83a51932fb19198c64950d
John Mahowald <john.mahowald@gmail.com>
2015-03-11 00:41:55 -0700

    When a user logs in, their username is on the homepage

--- spec/features/root_spec.rb
+++ spec/features/root_spec.rb
@@ -4,11 +4,14 @@ RSpec.describe "Signing In" do
   it "can sign a user in" do
     @user = create :user
 
+    expect(page.has_button? "Sign out").to be false
+
     visit("/")
     fill_in("session[username]", with: @user.username)
     fill_in("session[password]", with: @user.password)
     click_button("Log in")
 
-    expect(page).to have_content "Hello World"
+    expect(page.has_button? "Sign out").to be true
+    expect(page).to have_content @user.username
   end
 end
